d0dc1459a0cbca0f6ecb43dc1f0511d37da6aa30
Mads Kristensen <post@madskristensen.net>
2017-10-20 20:56:25 -0700

    Added comment.js and print CSS

---
+++ src/wwwroot/js/comments.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    // Expand comment form
+    var content = document.querySelector("#comments textarea");
+
+    if (content) {
+        content.addEventListener("focus", function () {
+            document.querySelector(".details").className += " show";
+        }, false);
+    }
+})();
