35d643bc5f4784b4a67218f60f9e621e0559c3b4
Mads Kristensen <post@madskristensen.net>
2017-10-29 06:49:06 -0700

    Using utf-8 for gravatar generation Fixed #20

--- src/Models/Comment.cs
+++ src/Models/Comment.cs
@@ -27,7 +27,7 @@ namespace Miniblog.Core.Models
         {
             using (var md5 = System.Security.Cryptography.MD5.Create())
             {
-                byte[] inputBytes = Encoding.ASCII.GetBytes(Email.Trim().ToLowerInvariant());
+                byte[] inputBytes = Encoding.UTF8.GetBytes(Email.Trim().ToLowerInvariant());
                 byte[] hashBytes = md5.ComputeHash(inputBytes);
 
                 // Convert the byte array to hexadecimal string
