633a2e7111887b7fc50be87a650ff50313a850a7
Mads Kristensen <post@madskristensen.net>
2017-11-03 09:52:25 -0700

    Removed old string from controller

--- src/Controllers/BlogController.cs
+++ src/Controllers/BlogController.cs
@@ -25,7 +25,7 @@ namespace Miniblog.Core.Models
         public async Task<IActionResult> Index([FromRoute]int page = 0)
         {
             var posts = await _blog.GetPosts(_settings.Value.PostsPerPage, _settings.Value.PostsPerPage * page);
-            ViewData["Title"] = _settings.Value.Name + " - A blog about ASP.NET & Visual Studio";
+            ViewData["Title"] = _settings.Value.Name;
             ViewData["Description"] = _settings.Value.Description;
             ViewData["prev"] = $"/{page + 1}/";
             ViewData["next"] = $"/{(page <= 1 ? null : page - 1 + "/")}";
