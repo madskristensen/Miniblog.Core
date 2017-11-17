f62d7aacd944c920fa251492c86e23cabe3c6ef3
Thomas Pleasance <me@thomaspleasance.com>
2017-11-17 20:17:41 +0000

    First pass of Streamline authentication between AccountController and MetaWeblogService

---
+++ src/Services/BlogUserServices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.Extensions.Configuration;
+
+namespace Miniblog.Core.Services
+{
+    public class BlogUserServices: IUserServices
+    {
+        private readonly IConfiguration _config;
+
+        public BlogUserServices(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public bool ValidateUser(string username, string password)
+        {
+            return username == _config["user:username"] && VerifyHashedPassword(password, _config);
+        }
+
+        private bool VerifyHashedPassword(string password, IConfiguration config)
+        {
+            byte[] saltBytes = Encoding.UTF8.GetBytes(config["user:salt"]);
+
+            byte[] hashBytes = KeyDerivation.Pbkdf2(
+                password: password,
+                salt: saltBytes,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 1000,
+                numBytesRequested: 256 / 8
+            );
+
+            string hashText = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+            return hashText == config["user:password"];
+        }
+    }
+}
---
+++ src/Services/IUserServices.cs
@@ -0,0 +1,7 @@
+﻿namespace Miniblog.Core.Services
+{
+    public interface IUserServices
+    {
+        bool ValidateUser(string username, string password);
+    }
+}
