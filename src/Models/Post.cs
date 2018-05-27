69018d9a1c68cea27c39a4338f748ef58e8b2145
dyardyGIT <dyardy@hotmail.com>
2018-05-26 22:17:03 -0400

    Only perform result.replace if result !=null
    Exception was thrown if result was empty.

--- src/Models/Post.cs
+++ src/Models/Post.cs
@@ -84,14 +84,15 @@ namespace Miniblog.Core.Models
         public string RenderContent()
         {
             var result = Content;
+            if (!string.IsNullOrEmpty(result))
+            {
+                // Set up lazy loading of images/iframes
+                result = result.Replace(" src=\"", " src=\"data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==\" data-src=\"");
 
-            // Set up lazy loading of images/iframes
-            result = result.Replace(" src=\"", " src=\"data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==\" data-src=\"");
-
-            // Youtube content embedded using this syntax: [youtube:xyzAbc123]
-            var video = "<div class=\"video\"><iframe width=\"560\" height=\"315\" title=\"YouTube embed\" src=\"about:blank\" data-src=\"https://www.youtube-nocookie.com/embed/{0}?modestbranding=1&amp;hd=1&amp;rel=0&amp;theme=light\" allowfullscreen></iframe></div>";
-            result = Regex.Replace(result, @"\[youtube:(.*?)\]", m => string.Format(video, m.Groups[1].Value));
-
+                // Youtube content embedded using this syntax: [youtube:xyzAbc123]
+                var video = "<div class=\"video\"><iframe width=\"560\" height=\"315\" title=\"YouTube embed\" src=\"about:blank\" data-src=\"https://www.youtube-nocookie.com/embed/{0}?modestbranding=1&amp;hd=1&amp;rel=0&amp;theme=light\" allowfullscreen></iframe></div>";
+                result = Regex.Replace(result, @"\[youtube:(.*?)\]", m => string.Format(video, m.Groups[1].Value));
+            }
             return result;
         }
     }
