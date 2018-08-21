9a01b3c42667898c6c77c57a16780ccf87c70377
Torsten Schuster <tsc@sutor.ws>
2018-08-21 13:04:23 +0200

    fixes small lint suggestion and removed a wrong comment for cdn on tinymce

--- src/wwwroot/js/admin.js
+++ src/wwwroot/js/admin.js
@@ -1,10 +1,56 @@
 ﻿(function () {
 
-    var edit = document.getElementById("edit");
+    // File upload
+    function handleFileSelect(event) {
+        if (window.File && window.FileList && window.FileReader) {
 
-    if (edit) {
-        // Setup editor
-        var editPost = document.getElementById("Content");
+            var files = event.target.files;
+
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+
+                // Only image uploads supported
+                if (!file.type.match('image'))
+                    continue;
+
+                var reader = new FileReader();
+                reader.addEventListener("load", function (event) {
+                    var image = new Image();
+                    image.alt = file.name;
+                    image.onload = function (e) {
+                        image.setAttribute("data-filename", file.name);
+                        image.setAttribute("width", image.width);
+                        image.setAttribute("height", image.height);
+                        tinymce.activeEditor.execCommand('mceInsertContent', false, image.outerHTML);
+                    };
+                    image.src = this.result;
+
+                });
+
+                reader.readAsDataURL(file);
+            }
+
+            document.body.removeChild(event.target);
+        }
+        else {
+            console.log("Your browser does not support File API");
+        }
+    }
+
+    // remove empty strings
+    function removeEmpty(item) {
+        var trimmedItem = item.trim();
+        if (trimmedItem.length > 0) {
+            return trimmedItem;
+        }
+    }
+
+    // edit form
+    var edit = document.getElementById("edit");
+    // Setup editor
+    var editPost = document.getElementById("Content");
+
+    if (edit && editPost) {
 
         if (typeof window.orientation !== "undefined" || navigator.userAgent.indexOf('IEMobile') !== -1) {
             tinymce.init({
@@ -57,43 +103,6 @@
                 }
             });
         }
-
-        // File upload
-        function handleFileSelect(event) {
-            if (window.File && window.FileList && window.FileReader) {
-
-                var files = event.target.files;
-
-                for (var i = 0; i < files.length; i++) {
-                    var file = files[i];
-
-                    // Only image uploads supported
-                    if (!file.type.match('image'))
-                        continue;
-
-                    var reader = new FileReader();
-                    reader.addEventListener("load", function (event) {
-                        var image = new Image();
-                        image.alt = file.name;
-                        image.onload = function (e) {
-                            image.setAttribute("data-filename", file.name);
-                            image.setAttribute("width", image.width);
-                            image.setAttribute("height", image.height);
-                            tinymce.activeEditor.execCommand('mceInsertContent', false, image.outerHTML);
-                        };
-                        image.src = this.result;
-
-                    });
-
-                    reader.readAsDataURL(file);
-                }
-
-                document.body.removeChild(event.target);
-            }
-            else {
-                console.log("Your browser does not support File API");
-            }
-        }
     }
 
     // Delete comments
@@ -115,24 +124,17 @@
     var categories = document.getElementById("categories");
     if (selecttag && categories) {
 
-        function removeEmpy(item) {
-            var trimmedItem = item.trim();
-            if (trimmedItem.length > 0) {
-                return trimmedItem
-            }
-        }
-
         selecttag.onchange = function () {
 
             var phv = selecttag.placeholder;
             var val = selecttag.value.toLowerCase();
 
             var phv_array = phv.split(",").map(function (item) {
-                return removeEmpy(item);
+                return removeEmpty(item);
             });
 
             var val_array = val.split(",").map(function (item) {
-                return removeEmpy(item);
+                return removeEmpty(item);
             });
 
             for (var j = val_array.length - 1; j >= 0; j--) {
