056964f01556b04c3bc1a6a78b07f1e25508fad0
Torsten Schuster <tsc@sutor.ws>
2018-08-27 12:12:00 +0200

    added archive view functionality to blog

---
+++ src/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Miniblog.Core.Models
+{
+    public class CategoryCount
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public int Count { get; set; } = 0;
+    }
+
+    public class Category
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public int Count { get; set; } = 0;
+
+        [Required]
+        public IList<Post> Posts { get; set; } = new List<Post>();
+    }
+}
