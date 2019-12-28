a053f2fb9d2cc0f19e173bdd7ddb8143412f72ed
@bacardibryant <bacardi.bryant@outlook.com>
2019-12-28 04:12:25 -0600

    Restore appsettings.json file to upstream and reset defaults on BlogSettings.cs.

--- src/BlogSettings.cs
+++ src/BlogSettings.cs
@@ -2,7 +2,7 @@
 {
     public class BlogSettings
     {
-        public string Owner { get; set; } = "Mads Kristensen";
+        public string Owner { get; set; } = "The Owner";
         public int PostsPerPage { get; set; } = 2;
         public int CommentsCloseAfterDays { get; set; } = 10;
     }
