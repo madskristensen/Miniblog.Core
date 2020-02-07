e1b90376894d69bd5022e4e0b14bc3d893708eae
Felony <onyfel@gmail.com>
2020-02-07 10:03:20 +0330

    Add LiteDatabaseBlogService

---
+++ src/Services/LiteDatabaseBlogService.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Miniblog.Core.Models;
+
+namespace Miniblog.Core.Services
+{
+    public class LiteDatabaseBlogService : IBlogService
+    {
+        public Task<IEnumerable<Post>> GetPosts()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Post>> GetPosts(int count, int skip = 0)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Post>> GetPostsByCategory(string category)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Post> GetPostBySlug(string slug)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Post> GetPostById(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetCategories()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task SavePost(Post post)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeletePost(Post post)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<string> SaveFile(byte[] bytes, string fileName, string suffix = null)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
