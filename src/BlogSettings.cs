561e5b3f39a03dbe535910e9373c5b7c16d9a74f
KevBat <kbathrick1@gmail.com>
2021-01-15 14:24:56 -0500

    Add setting to display comments (#157)

--- src/BlogSettings.cs
+++ src/BlogSettings.cs
@@ -4,6 +4,8 @@ namespace Miniblog.Core
     {
         public int CommentsCloseAfterDays { get; set; } = 10;
 
+        public bool DisplayComments { get; set; } = true;
+
         public PostListView ListView { get; set; } = PostListView.TitlesAndExcerpts;
 
         public string Owner { get; set; } = "The Owner";
